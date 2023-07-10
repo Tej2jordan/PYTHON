af84fa5e7d1569cabec4bd5abb79200357250390
Tej2jordan <128887400+Tej2jordan@users.noreply.github.com>
2023-07-11 00:18:22 +0530

    riddle game using python
    riddle game using python

---
+++ riddle game.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+choice1 = input(
+    "You are in a jungle full of monsters you want to get out of this jungle for this choose from the below option where you will go 'left' or 'right'? "
+).lower()
+if choice1 == 'left':
+    choice2 = input(
+        "you came to a lake. there is an iland in the middle of the lake. type 'wait' to wait for a boat or type 'swim' to swim through."
+    ).lower()
+    if choice2 == 'wait':
+        choice3 = input(
+            "You came to the temple by a boat, there a three door 'red', 'green', 'blue' which one you will choose?"
+        ).lower()
+        if choice3 == 'green':
+            print("congradulation you found a way out of this jungle, enjoy")
+        elif choice3 == 'red' or 'blue':
+            print("You have failed this city, you die.....")
+
+    else:
+        print('Game over buddy, you are eaten by a crocodile')
+else:
+    print('game over buddy,you bin eaten by lion')
